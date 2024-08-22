a3bfe939b2a39d54b117376106d69c95d6a4a6ac
igromanru <igromanru@yahoo.de>
2024-08-22 17:44:46 +0200

    playing around with AIController and Leyak

--- scripts/main.lua
+++ scripts/main.lua
@@ -32,18 +32,55 @@ end
 
 RegisterKeyBind(Key.L, function()
     ExecuteInGameThread(function()
-        local myPlayer = AFUtils.GetMyPlayer()
-        if myPlayer then
-            local currentItem = myPlayer.ItemInHand_BP
-            if currentItem:IsValid() then
-                local itemData = currentItem.ItemData
-                local changeableData = currentItem.ChangeableData
-                LogDebug("ItemInHand:")
-                LogDebug("ItemName: " .. itemData.ItemName_51_B88648C048EE5BC2885E4E95F3E13F0A:ToString())
+        LogDebug("------------ L ---------------")
+        local aiDirector = FindFirstOf("Abiotic_AIDirector_C")
+        if aiDirector and aiDirector:IsValid() then
+            LogDebug("-- Abiotic_AIDirector_C:")
+            AFUtils.LogAIDirector(aiDirector)
+            local timerHandle = { Handle = aiDirector.LeyakTimer.Handle }
+            LogDebug("LeyakTimer IsValid: "..tostring(GetKismetSystemLibrary():K2_IsValidTimerHandle(timerHandle)))
+            timerHandle.Handle = aiDirector.CoworkerTimer.Handle
+            LogDebug("CoworkerTimer IsValid: "..tostring(GetKismetSystemLibrary():K2_IsValidTimerHandle(timerHandle)))
+            if aiDirector.ActiveLeyak:IsValid() then
+                LogDebug("-- ActiveLeyak:")
+                AFUtils.LogNPCLeyak(aiDirector.ActiveLeyak)
 
-                AFUtils.LogInventoryChangeableDataStruct(changeableData, "ChangeableData.")
+                -- aiDirector.ActiveLeyak:DropEssence()
+            else
+                local spawnDistanceInM = 20
+                local playerController = GetMyPlayerController()
+                if playerController and playerController.PlayerCameraManager:IsValid() then
+                    local cameraManager = playerController.PlayerCameraManager
+                    local lookDirection = cameraManager:GetActorForwardVector()
+                    local lookDirOffset = GetKismetMathLibrary():Multiply_VectorFloat(lookDirection, MToUnits(spawnDistanceInM))
+                    local spawnLocation = GetKismetMathLibrary():Add_VectorVector(cameraManager:GetCameraLocation(), lookDirOffset)
+                    aiDirector:SpawnLeyak(VectorToUserdata(spawnLocation));
+                end
             end
+        else
+            LogDebug("No Abiotic_AIDirector_C found")
         end
+        local aiControllerLeyak = FindFirstOf("AI_Controller_Leyak_C")
+        if aiControllerLeyak and aiControllerLeyak:IsValid() then
+            LogDebug("-- AI_Controller_Leyak_C:")
+            AFUtils.LogAIControllerLeyak(aiControllerLeyak)
+            -- aiControllerLeyak.WantsToDespawn = true
+        else
+            LogDebug("No AI_Controller_Leyak_C found")
+        end
+        -- local myPlayer = AFUtils.GetMyPlayer()
+        -- if myPlayer then
+        --     local currentItem = myPlayer.ItemInHand_BP
+        --     if currentItem:IsValid() then
+        --         local itemData = currentItem.ItemData
+        --         local changeableData = currentItem.ChangeableData
+        --         LogDebug("ItemInHand:")
+        --         LogDebug("ItemName: " .. itemData.ItemName_51_B88648C048EE5BC2885E4E95F3E13F0A:ToString())
+
+        --         AFUtils.LogInventoryChangeableDataStruct(changeableData, "ChangeableData.")
+        --     end
+        -- end
+        LogDebug("------------------------------")
     end)
 end)
 
@@ -69,48 +106,55 @@ end)
 
 RegisterKeyBind(Key.U, function()
     ExecuteInGameThread(function()
-        
+        LogDebug("------------ U ---------------")
+        local aiControllerLeyak = FindFirstOf("AI_Controller_Leyak_C")
+        if aiControllerLeyak and aiControllerLeyak:IsValid() then
+            aiControllerLeyak:Despawn()
+        else
+            LogDebug("No AI_Controller_Leyak_C found")
+        end
+        LogDebug("------------------------------")
     end)
 end)
 
-RegisterHook("/Game/Blueprints/Characters/Abiotic_PlayerCharacter.Abiotic_PlayerCharacter_C:GetCurrentHeldItem", function(Context, Success, ItemSlotInfo, ItemData, Blueprint)
-    local playerCharacter = Context:get()
-    local success = Success:get()
-    local itemSlotInfo = ItemSlotInfo:get()
-    local itemData = ItemData:get()
-    local blueprint = Blueprint:get() -- AAbiotic_Item_ParentBP_C
+-- RegisterHook("/Game/Blueprints/Characters/Abiotic_PlayerCharacter.Abiotic_PlayerCharacter_C:GetCurrentHeldItem", function(Context, Success, ItemSlotInfo, ItemData, Blueprint)
+--     local playerCharacter = Context:get()
+--     local success = Success:get()
+--     local itemSlotInfo = ItemSlotInfo:get()
+--     local itemData = ItemData:get()
+--     local blueprint = Blueprint:get() -- AAbiotic_Item_ParentBP_C
 
-    -- if success then
-    --     LogDebug("[GetCurrentHeldItem] called:")
-    --     LogDebug("Success: " .. tostring(success))
+--     if success then
+--         LogDebug("[GetCurrentHeldItem] called:")
+--         LogDebug("Success: " .. tostring(success))
 
-    --     LogDebug("CurrentHotbarSlotSelected.Index: " ..  playerCharacter.CurrentHotbarSlotSelected.Index_5_6BDC7B3944A5DE0B319F9FA20720872F)
-    --     LogDebug("LastSelectedHotbarSlot: " ..  playerCharacter.LastSelectedHotbarSlot)
-    --     -- AFUtils.LogInventoryItemSlotStruct(itemSlotInfo, "ItemSlotInfo.")
-    --     -- LogDebug("---")
-    --     -- AFUtils.LogInventoryItemStruct(itemData, "ItemData.")
-    --     -- LogDebug("---")
-    --     -- AFUtils.LogInventoryItemStruct(playerCharacter.CurrentHeldItemData, "CurrentHeldItemData.")
-    --     LogDebug("---")
-    --     if blueprint:IsValid() then
-    --         LogDebug("Blueprint: " .. blueprint:GetClass():GetFullName())
-    --         if blueprint:IsA(AFUtils.GetClassAbiotic_Item_ParentBP_C()) then
-    --             AFUtils.LogItemParentBP(blueprint, "Blueprint.")
-    --             LogDebug("---")
-    --             -- AFUtils.LogInventoryItemStruct(blueprint.ItemData, "Blueprint.ItemData.")
-    --             -- AFUtils.LogInventoryChangeableDataStruct(blueprint.ChangeableData, "Blueprint.ChangeableData.")
-    --         end
-    --         -- LogDebug("BlueprintCreatedComponents Num: " .. #blueprint.BlueprintCreatedComponents)
-    --         -- for i = 1, #blueprint.BlueprintCreatedComponents, 1 do
-    --         --     local component = blueprint.BlueprintCreatedComponents[i]
-    --         --     if component:IsValid() then
-    --         --         LogDebug(i .. ": " .. component:GetClass():GetFullName())
-    --         --     end
-    --         -- end
-    --     end
-    --     LogDebug("------------------------------")
-    -- end
-end)
+--         LogDebug("CurrentHotbarSlotSelected.Index: " ..  playerCharacter.CurrentHotbarSlotSelected.Index_5_6BDC7B3944A5DE0B319F9FA20720872F)
+--         LogDebug("LastSelectedHotbarSlot: " ..  playerCharacter.LastSelectedHotbarSlot)
+--         -- AFUtils.LogInventoryItemSlotStruct(itemSlotInfo, "ItemSlotInfo.")
+--         -- LogDebug("---")
+--         -- AFUtils.LogInventoryItemStruct(itemData, "ItemData.")
+--         -- LogDebug("---")
+--         -- AFUtils.LogInventoryItemStruct(playerCharacter.CurrentHeldItemData, "CurrentHeldItemData.")
+--         LogDebug("---")
+--         if blueprint:IsValid() then
+--             LogDebug("Blueprint: " .. blueprint:GetClass():GetFullName())
+--             if blueprint:IsA(AFUtils.GetClassAbiotic_Item_ParentBP_C()) then
+--                 AFUtils.LogItemParentBP(blueprint, "Blueprint.")
+--                 LogDebug("---")
+--                 -- AFUtils.LogInventoryItemStruct(blueprint.ItemData, "Blueprint.ItemData.")
+--                 -- AFUtils.LogInventoryChangeableDataStruct(blueprint.ChangeableData, "Blueprint.ChangeableData.")
+--             end
+--             -- LogDebug("BlueprintCreatedComponents Num: " .. #blueprint.BlueprintCreatedComponents)
+--             -- for i = 1, #blueprint.BlueprintCreatedComponents, 1 do
+--             --     local component = blueprint.BlueprintCreatedComponents[i]
+--             --     if component:IsValid() then
+--             --         LogDebug(i .. ": " .. component:GetClass():GetFullName())
+--             --     end
+--             -- end
+--         end
+--         LogDebug("------------------------------")
+--     end
+-- end)
 
 -- RegisterHook("/Game/Blueprints/Characters/Abiotic_InventoryComponent.Abiotic_InventoryComponent_C:GetItemInSlot", function(Context, Index, Success, SlotData, ItemData)
 --     local this = Context:get()
@@ -125,6 +169,47 @@ end)
 --     AFUtils.LogInventoryItemSlotStruct(itemSlotInfo, "SlotData.")
 --     LogDebug("---")
 --     AFUtils.LogInventoryItemStruct(itemData, "ItemData.")
+--     LogDebug("------------------------------")
+-- end)
+
+RegisterHook("/Game/Blueprints/Environment/Systems/Abiotic_AIDirector.Abiotic_AIDirector_C:SpawnLeyak", function(Context, Location)
+    local aIDirector = Context:get()
+    local location = Location:get()
+
+    LogDebug("[SpawnLeyak] called:")
+    AFUtils.LogAIDirector(aIDirector)
+    local leakTimer = {
+        Handle = aIDirector.LeyakTimer.Handle
+    }
+    
+    LogDebug("------------------------------")
+end)
+
+RegisterHook("/Game/Blueprints/Environment/Systems/Abiotic_AIDirector.Abiotic_AIDirector_C:LeyakFailsafeRemove", function(Context)
+    local aIDirector = Context:get()
+
+    LogDebug("[Abiotic_AIDirector_C:LeyakFailsafeRemove] called:")
+    LogDebug("------------------------------")
+end)
+
+RegisterHook("/Game/Blueprints/Characters/NPCs/AI_Controller_Leyak.AI_Controller_Leyak_C:Despawn", function(Context)
+    local aiControllerLeyak = Context:get()
+
+    LogDebug("[AI_Controller_Leyak_C:Despawn] called:")
+    LogDebug("------------------------------")
+end)
+
+RegisterHook("/Game/Blueprints/Characters/NPCs/NPC_Leyak.NPC_Leyak_C:OnDespawn", function(Context)
+    local aiControllerLeyak = Context:get()
+
+    LogDebug("[NPC_Leyak_C:OnDespawn] called:")
+    LogDebug("------------------------------")
+end)
+
+-- RegisterHook("/Game/Blueprints/Characters/NPCs/NPC_Leyak.NPC_Leyak_C:ReceiveTick", function(Context, DeltaSeconds)
+--     local aiControllerLeyak = Context:get()
+
+--     LogDebug("[NPC_Leyak_C:ReceiveTick] called:")
 --     LogDebug("------------------------------")
 -- end)
 
